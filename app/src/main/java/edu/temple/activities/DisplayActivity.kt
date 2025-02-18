d86a9eb50310884253fc50dd4d0b33e227e65933
grantmerrigan <gmerrigan13@gmail.com>
2025-02-18 12:20:39 -0500

    Added fontsizeadapter

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -5,13 +5,14 @@ import android.os.Bundle
 import android.widget.TextView
 
 class DisplayActivity : AppCompatActivity() {
+    public val SIZE_KEY = "fontSize"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display)
 
         // TODO Step 3: Extract transferred value and use for lyricsDisplayView text size
         with (findViewById<TextView>(R.id.lyricsDisplayTextView)) {
-
+            intent.getFloatExtra(SIZE_KEY, 22f)
         }
 
 
